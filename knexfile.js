8ccdf96cc3e3cdd429bde550c44eff961c8f06b4
Bolawole <andubolawole@gmail.com>
2022-08-24 09:31:44 -0400

    Update README.md

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,24 @@
+// const connections = {
+//   development: {
+//     client: 'mysql',
+//     connection: {
+//       host: '127.0.0.1',
+//       user: 'root',
+//       password: 'rootroot',
+//       database: 'nota_bene',
+//     },
+//   },
+//   production: {
+//     client: 'mysql',
+//     connection: process.env.JAWSDB_URL,
+//   },
+// };
+
+// module.exports = 
+//   process.env.NODE_ENV === 'production'
+//     ? connections.production
+//     : connections.development;
+
 module.exports = {
   development: {
     client: 'mysql',
